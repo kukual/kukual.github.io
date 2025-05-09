61737ebdd76405186e14c6e40402b9821fdc22f0
kukual <lmjycyzmxqhdx@163.com>
2025-05-09 16:54:49 +0800

    Site updated: 2025-05-09 16:54:47

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
+			//apiPath: "https://github.com/kukual/kukual.github.io/tree/main/live2d_models/",
 			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
